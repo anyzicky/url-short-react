79f29d6285b476d00ef9c74471f47892e3ab2cb9
Denis Suraev <any.zicky@gmail.com>
2017-02-27 19:53:27 +0300

    start learn

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+        <div className="container">
+
+          <div className="starter-template">
+            <h1>Bootstrap starter template</h1>
+        <p className="lead">Use this document as a way to quickly start any new project.<br/> All you get is this text and a mostly barebones HTML document.</p>
+          </div>
+
+        </div>
+    );
+  }
+}
+
+export default App;
