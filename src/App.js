d2f240c4f489aa26e65572e90c1bb5deb0101b80
Denis Suraev <any.zicky@gmail.com>
2017-02-28 08:00:47 +0300

    prepare app struct, create ui app, add react url component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
 import React, { Component } from 'react';
 import './App.css';
+import Url from './Url';
 
 class App extends Component {
   render() {
     return (
         <div className="container">
 
-          <div className="starter-template">
-            <h1>Bootstrap starter template</h1>
-        <p className="lead">Use this document as a way to quickly start any new project.<br/> All you get is this text and a mostly barebones HTML document.</p>
+
+          <div className="jumbotron">
+            <h1>Put long url here</h1>
+            <div className="row">
+              <div className="col-xs-12">
+                <input type="text" className="form-control url" placeholder=".col-xs-2"/>
+              </div>
+
+            </div>
+            <p><a className="btn btn-lg btn-success" href="#" role="button">Short me</a></p>
+          </div>
+
+          <div className="row marketing">
+
+            <Url/>
+
+            <Url/>
           </div>
 
         </div>
